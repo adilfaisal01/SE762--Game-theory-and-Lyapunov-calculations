e7e53baaa4bc33cc8b34b0403cda3c1a5035c49a
Adil Faisal <adilfaisal01@gmail.com>
2025-08-30 19:34:37 -0400

    Mabrok framework

--- se762_code.py
+++ se762_code.py
@@ -9,43 +9,12 @@ import matplotlib.pyplot as plt
 
 #Assumptions: 1. probabilities are non-negative and between (0,1)
 
-def normalizing_probabilities(strat):
-    strat=np.clip(strat,0,1)
-    total=np.sum(strat)
-    return strat/total if total!=0 else 1
+# def uncertainty(delta):
+#     return None
 
+def gibbs(u):
+    return np.exp(u)/np.sum(np.exp(u))
 
-def noisyperturbations(EQ_pts,std):
-
-# std represents the scale of the initial disturbance being observed
-    noise=np.random.normal(loc=0,scale=std,size=len(EQ_pts[0:3]))
-    noisydit_ST=EQ_pts[0:3]+noise
-    noisydit_ST=normalizing_probabilities(noisydit_ST)
-    # print(f'Striker initial condition: {np.round(noisydit,decimals=3)}')
-
-    noise_GK=np.random.normal(loc=0,scale=std,size=len(EQ_pts[3:6]))
-    noisydit_GK=EQ_pts[3:6]+noise_GK
-    noisydit_GK=normalizing_probabilities(noisydit_GK)
-    
-    # print(f'GK initial condition: {np.round(noisydit_GK,decimals=3)}')
-
-    return np.concatenate((noisydit_ST,noisydit_GK),axis=None)
-
-# Plotting the KL divergence since it is the Lyapunov candidate
-
-def KL_divergence(EQ_pts,soln_n,t):
-
-    V=[]
-    dvdt=[]
-    for i in range(len(EQ_pts)):
-        v=EQ_pts[i]*np.log(EQ_pts[i]/soln_n[i])
-        V.append(v)
-        vdot=np.gradient(v,t)
-        dvdt.append(vdot)
-
-    VKL=np.sum(V,axis=0)
-    dvdt=np.sum(dvdt,axis=0)
-    return VKL,dvdt
 
 class SoccerGame:
 # zero-sum non cooperative game between striker and goalkeeper
@@ -56,53 +25,9 @@ class SoccerGame:
         else:
             self.B = gk_payoff
 
-    def replicator_dynamics(self,z):
-        x=np.array(z[0:3]) #Striker strategy set
-        x=np.reshape(x,(3,1))
-        y=np.array(z[3:6]) #GK strategy set
-        y=np.reshape(y,(3,1))
-        dxdt=np.zeros(shape=(3,1)) # Striker replicator dynamics
-        dydt=np.zeros(shape=(3,1))
+    def payoffs(self,x,y):
+        striker_payoffs=self.A@y
+        gk_payoffs=self.B@x
+        return striker_payoffs,gk_payoffs
     
-
-        # x and y are column vectors
-
-        Ay=np.matmul(self.A,y) # strriker payoff matrix
-        phi=np.matmul(x.T,Ay) # expected value for the striker, average payoff
-        Bx=np.matmul(self.B,x)
-        phi_2=np.matmul(y.T,Bx)
-
-        for i in range(0,3):
-            dxdt[i]=x[i]*(Ay[i]-phi[0,0])
-            dydt[i]=y[i]*(Bx[i]-phi_2[0,0])
-
-        return np.concatenate((dxdt,dydt),axis=None)
-# print(replicator_dynamics(z=[0.45,0.05,0.5,0.3,0.6,0.1]))
-
-
-    def solving_replicator_dynamics(self,initial_guess):
-        EQ_pts= fsolve(self.replicator_dynamics,x0=initial_guess) #find the equilibrium point of the replicator dynamics
-        EQ_pts[:3]=normalizing_probabilities(EQ_pts[:3])
-        EQ_pts[3:]=normalizing_probabilities(EQ_pts[3:])
-        # print(f"Replicator dynamics equilibrium points striker: {np.array([EQ_pts[0],EQ_pts[1],EQ_pts[2]])}")
-        # print(f"Replicator dynamics equilibrium points GK: {np.array([EQ_pts[3],EQ_pts[4],EQ_pts[5]])}")
-        self.EQ_pts=EQ_pts
-        return EQ_pts
-
-
-
-
-# since the game is non-cooperative, it means that there is at least one Nash equilibrium as proved by Nash
-
-
-    def replicator_dynamics_time(self,t,z):
     
-        return self.replicator_dynamics(z)
-    
-
-    ## using Feedback Linearization to make the replicator dynamics asymptotically stable
-
-    def feedback_linearization(self,t,z):
-        dxdt_controlled=-0.5*(z-self.EQ_pts)
-        return dxdt_controlled
-
--- testingcode.py
+++ testingcode.py
@@ -1,34 +1,62 @@
-from se762_code import SoccerGame,noisyperturbations,normalizing_probabilities
+from se762_code import SoccerGame,gibbs
 import numpy as np
-A=np.array([[0,0.8,0.7],[0.9,0,0.2],[0.75,0.45,0]])
-x0=[0.3,0.3,0.4,0.3,0.3,0.4]
-game=SoccerGame(striker_payoff=A)
-eq_pts=game.solving_replicator_dynamics(initial_guess=x0)
-
-Noisestarts=noisyperturbations(eq_pts,std=0.05)
-t_eval=np.linspace(0,100,2500)
-from scipy.integrate import solve_ivp
-solutions=solve_ivp(game.replicator_dynamics_time,t_span=(0,100),y0=Noisestarts,t_eval=np.linspace(0,100,2500),atol=1e-8,rtol=1e-6)
-
-FL_solutions=solve_ivp(game.feedback_linearization,t_span=(0,100),y0=Noisestarts,t_eval=np.linspace(0,100,2500),atol=1e-8,rtol=1e-6)
-
 import matplotlib.pyplot as plt
-plt.plot(t_eval,FL_solutions.y[0],color='r')
-plt.axhline(y=eq_pts[0],ls='--',color='r')
+import nashpy as nash
 
-plt.plot(t_eval,FL_solutions.y[1],color='b')
-plt.axhline(y=eq_pts[1],ls='--',color='b')
+A=np.array([[0,0.8,0.7],[0.9,0,0.2],[0.75,0.45,0]])
+soccergame=SoccerGame(striker_payoff=A)
+B=soccergame.B
+Game=nash.Game(A,B)
 
-plt.plot(t_eval,FL_solutions.y[2],color='k')
-plt.axhline(y=eq_pts[2],ls='--',color='k')
+nash_eq=Game.support_enumeration() #Nash equilibrium of the game
+optimalpoints=list(nash_eq)[0][:][0]
 
-plt.plot(t_eval,FL_solutions.y[3],color='g')
-plt.axhline(y=eq_pts[3],ls='--',color='g')
+x=np.array([[0.3],[0.3],[0.4]])
+y=np.array([[0.3],[0.3],[0.4]])
 
-plt.plot(t_eval,FL_solutions.y[4],color='m')
-plt.axhline(y=eq_pts[4],ls='--',color='m')
+sp,gp=soccergame.payoffs(x,y) #striker and goalkeeper payoffs
 
-plt.plot(t_eval,FL_solutions.y[5],color='y')
-plt.axhline(y=eq_pts[5],ls='--',color='y')
+T=60 #simulation time
+n=100#number of simulation steps
+time_array=np.linspace(0,T,n)
+u=np.zeros((3,1))
+v=np.zeros((3,1))
+xhistory=[]
+yhistory=[]
+dt=T/n
+xhistory.append(x.flatten())
+yhistory.append(y.flatten())
 
+
+for t in time_array:
+    strikerpayoff,goalkeeperpayoff=soccergame.payoffs(x,y) #payoff, 3x1 which will be input as P=udot in Mabrok's paper
+    # T=P-1*x
+    # integrator (1/s)
+    u=u+strikerpayoff*dt
+    v=v+goalkeeperpayoff*dt
+
+    #strategy being updated
+    x=gibbs(u)
+    y=gibbs(v)
+
+    # storing the history
+    xhistory.append(x.flatten())
+    yhistory.append(y.flatten())
+
+xhistory=np.array(xhistory)
+yhistory=np.array(yhistory)
+time_array= np.concatenate([[0], time_array])
+
+plt.figure()
+plt.plot(time_array,yhistory[:,0],label='rock')
+plt.plot(time_array,yhistory[:,1],label='paper')
+plt.plot(time_array,yhistory[:,2],label='scissors')
+plt.xlim((-0.1,61))
+plt.show()
+
+plt.figure()
+plt.plot(time_array,xhistory[:,0],label='rock')
+plt.plot(time_array,xhistory[:,1],label='paper')
+plt.plot(time_array,xhistory[:,2],label='scissors')
+plt.xlim((-0.1,61))
 plt.show()
